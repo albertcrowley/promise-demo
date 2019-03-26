f17564dd08c9ee1689b7ab022683997306c83a95
Al Crowley <albert.crowley@tcg.com>
2019-03-26 09:24:27 -0400

    ansync python

--- p-fetching.py
+++ p-fetching.py
@@ -3,25 +3,46 @@ import sqlite3
 import urllib
 import urllib.request
 import json
+import asyncio
 
 db_file = 'chinook.db'
 sql1 = "SELECT ArtistId, Name from artists limit 5"
 url =  "http://jsonplaceholder.typicode.com/todos/"
 sql2 = "SELECT Name from tracks where name like '%%%s%%' limit 10"
 
-
-conn = sqlite3.connect('./chinook.db')
-res = conn.execute(sql1)
-artists = res.fetchall()
-for (id,a) in artists:
+async def fetch(id):
     r = urllib.request.urlopen(url + str(id))
     js = r.read()
     title = (json.loads(js))['title']
     word = title.split(' ')[0]
-    print ("searching for " + word)
+    return word
 
-    res = conn.execute( sql2 % (word) )
+def lookup(fut):
+    print ("my result is " + fut.result())
+    conn = sqlite3.connect('./chinook.db')
+    res = conn.execute( sql2 % (fut.result()) )
     names = res.fetchall()
     for n in names:
         print ("Got a match on " + str(n))
 
+async def work():
+    conn = sqlite3.connect('./chinook.db')
+    res = conn.execute(sql1)
+    artists = res.fetchall()
+
+    tasks = []
+    for (id,a) in artists:
+        t = asyncio.create_task(fetch(id))
+        tasks.append( t )
+        t.add_done_callback( lookup )
+        print ("searching for " + str(id))
+
+
+    for t in tasks:
+        res = await t
+
+
+        #
+        #
+
+asyncio.run(work())
