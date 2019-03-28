d73cf65e7d44d23a4d9ce89d51d3c5b6b289173e
Al Crowley <albert.crowley@tcg.com>
2019-03-28 14:10:59 -0400

    synchronous fetch, oops!

--- p-fetching.py
+++ p-fetching.py
@@ -6,14 +6,14 @@ import json
 import asyncio
 
 db_file = 'chinook.db'
-sql1 = "SELECT ArtistId, Name from artists limit 5"
-url =  "http://jsonplaceholder.typicode.com/todos/"
+sql1 = "SELECT ArtistId, Name from artists limit 10"
+url =  "http://localhost:3000/artist/%s/word"
 sql2 = "SELECT Name from tracks where name like '%%%s%%' limit 10"
 
 async def fetch(id):
-    r = urllib.request.urlopen(url + str(id))
+    r = urllib.request.urlopen(url % str(id))
     js = r.read()
-    title = (json.loads(js))['title']
+    title = (json.loads(js))['word']
     word = title.split(' ')[0]
     return word
 
@@ -32,7 +32,7 @@ async def work():
 
     tasks = []
     for (id,a) in artists:
-        t = asyncio.create_task(fetch(id))
+        t = loop.create_task(fetch(id)) #this is synchronous!!!!
         tasks.append( t )
         t.add_done_callback( lookup )
         print ("searching for " + str(id))
@@ -45,4 +45,9 @@ async def work():
         #
         #
 
-asyncio.run(work())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(work())
+loop.close()
+
+# or in PYthon 3.7
+# asyncio.run(work())
