97ca4a7c3b6e72e20376fe3b1cd23c8072678ae5
Al Crowley <albert.crowley@tcg.com>
2019-03-28 11:27:25 -0400

    Now using local API

--- fetching.js
+++ fetching.js
@@ -4,8 +4,8 @@ const http = require('http')
 const fetch = require('node-fetch')
 
 const db_file = 'chinook.db'
-const sql1 = "SELECT ArtistId, Name from artists limit 5"
-const url =  'http://jsonplaceholder.typicode.com/todos/'
+const sql1 = "SELECT ArtistId, Name from artists limit 10"
+const url_template =  'http://localhost:3000/artist/:id/word'
 const sql2 = "SELECT Name from tracks where name like '%WORD%' limit 10"
 
 function using_callbacks() {
@@ -13,7 +13,7 @@ function using_callbacks() {
     db.all(sql1, {}, function (err, rows) {
         rows.forEach(function (artist) {
             console.log(artist.ArtistId + ": " + artist.Name);
-            http.get(url + artist.ArtistId, function (resp) {
+            http.get(url_template.replace(':id', artist.ArtistId), function (resp) {
                 let data = '';
 
                 // A chunk of data has been recieved.
@@ -25,7 +25,7 @@ function using_callbacks() {
                 resp.on('end', () => {
                     let result = JSON.parse(data);
 
-                    let word = result.title.split(" ")[0]
+                    let word = result.word
 
                     console.log("   - ", word)
 
@@ -46,15 +46,14 @@ function cleaner_using_callbacks() {
     const db = new sqlite3.Database(db_file);
     db.all(sql1, {}, function (err, rows) {
         rows.forEach(function (artist) {
-            http.get(url + artist.ArtistId, function (resp) {
+            http.get(url_template.replace(':id', artist.ArtistId), function (resp) {
                 let data = '';
                 resp.on('data', (chunk) => {
                     data += chunk;
                 });
                 resp.on('end', () => {
-                    let title = JSON.parse(data).title
-                    let first_word = title.split(" ")[0]
-                    db.all(sql2.replace("WORD", first_word), function (err, rows) {
+                    let word = JSON.parse(data).word
+                    db.all(sql2.replace("WORD", word), function (err, rows) {
                         rows.forEach(function (track) {
                             console.log(track.Name)
                         })
@@ -75,16 +74,22 @@ function using_promises() {
             return db.all(sql1)
         })
         .then( rows => {
-            const f = rows.map( row => fetch (url + row.ArtistId))
+            console.log("number of rows returned:", rows.length)
+            const f = rows.map( row => {
+                const url = url_template.replace(':id', row.ArtistId)
+                console.log ("fetching url", url)
+                return fetch (url)
+            })
             return Promise.all(f)
         })
         .then( (fetches) => {
             return (Promise.all( fetches.map( f => f.json()) ))
         })
         .then( jsons => {
-            return jsons.map( j => j.title.split(" ")[0])
+            return jsons.map( json => json.word)
         })
         .then(words => {
+            console.log ("top words",words)
             const queries = words.map( word => db.all(sql2.replace("WORD", word)))
             return Promise.all( queries )
         })
@@ -99,8 +104,8 @@ function using_promises() {
 
 
 // using_callbacks()
-cleaner_using_callbacks()
- using_promises()
+ cleaner_using_callbacks()
+// using_promises()
 
 
 
